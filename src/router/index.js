6053650bfe07c14f96f4622a507e3fda0e1dfadb
800300liu123 <982374602@qq.com>
2019-06-11 15:20:08 +0800

    新建登录分支新建登录组件配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Login from '@/components/login/login'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-
+      name:'login',
+      path: '/login',
+      component:Login
     }
   ]
 })
